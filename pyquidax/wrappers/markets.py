5f95e5a8bea40becb2e2da8aca693e588e056574
Blessing <sooperfect00@gmail.com>
2023-12-28 03:26:09 +0100

    Add AsyncMarket class

--- pyquidax/wrappers/markets.py
+++ pyquidax/wrappers/markets.py
@@ -1,6 +1,6 @@
 from typing import Optional, Literal
 
-from pyquidax.base import BaseAPIWrapper
+from pyquidax.base import BaseAPIWrapper, BaseAsyncAPIWrapper
 from pyquidax.utils import HTTPMethod, CurrencyPair, append_query_parameters
 
 
@@ -108,6 +108,112 @@ class Market(BaseAPIWrapper):
             query_params,
         )
         return self._api_call(
+            url=url,
+            method=HTTPMethod.GET,
+        )
+
+class AsyncMarket(BaseAsyncAPIWrapper):
+
+    async def all(self):
+        return await self._api_call(
+            url=f"{self.base_url}/markets",
+            method=HTTPMethod.GET,
+        )
+
+    async def tickers(self):
+        return await self._api_call(
+            url=f"{self.base_url}/markets/tickers",
+            method=HTTPMethod.GET,
+        )
+
+    async def get_ticker(self, pair: CurrencyPair):
+        return await self._api_call(
+            url=f"{self.base_url}/markets/tickers/{pair}",
+            method=HTTPMethod.GET,
+        )
+
+    async def get_k_line(
+        self,
+        pair: CurrencyPair,
+        timestamp: Optional[int] = None,
+        period: Optional[
+            Literal[1, 5, 15, 30, 60, 120, 240, 360, 720, 1440, 4320, 10080]
+        ] = None,
+        limit: Optional[int] = None,
+    ):
+        if limit:
+            if limit > 10_000:
+                raise ValueError("`limit` cannot be greater than `10_000`")
+        query_params = (
+            ("timestamp", timestamp),
+            ("period", period),
+            ("limit", limit),
+        )
+        url = append_query_parameters(f"{self.base_url}/markets/{pair}/k", query_params)
+        return await  self._api_call(
+            url=url,
+            method=HTTPMethod.GET,
+        )
+
+    async def get_k_line_with_pending_trades(
+        self,
+        pair: CurrencyPair,
+        trade_id: str,
+        limit: Optional[int] = None,
+        period: Optional[
+            Literal[1, 5, 15, 30, 60, 120, 240, 360, 720, 1440, 4320, 10080]
+        ] = None,
+        timestamp: Optional[int] = None,
+    ):
+        if limit:
+            if limit > 10_000:
+                raise ValueError("`limit` cannot be greater than `10_000`")
+        query_params = (
+            ("timestamp", timestamp),
+            ("period", period),
+            ("limit", limit),
+        )
+        url = append_query_parameters(
+            f"{self.base_url}/markets/{pair}/k_with_pending_trades/{trade_id}",
+            query_params,
+        )
+        return await self._api_call(
+            url=url,
+            method=HTTPMethod.GET,
+        )
+
+    async def get_order_book(
+        self,
+        pair: CurrencyPair,
+        ask_limit: Optional[int] = None,
+        bids_limit: Optional[int] = None,
+    ):
+        if ask_limit:
+            if ask_limit > 200:
+                raise ValueError("`ask_limit` cannot be greater than `200`")
+        if bids_limit:
+            if bids_limit > 200:
+                raise ValueError("`bids_limit` cannot be greater than `200`")
+        query_params = (
+            ("ask_limit", ask_limit),
+            ("bids_limit", bids_limit),
+        )
+        url = append_query_parameters(
+            f"{self.base_url}/markets/{pair}/order_book",
+            query_params,
+        )
+        return await self._api_call(
+            url=url,
+            method=HTTPMethod.GET,
+        )
+
+    async def get_depth_data(self, pair: CurrencyPair, limit: Optional[int] = None):
+        query_params = (("limit", limit),)
+        url = append_query_parameters(
+            f"{self.base_url}/markets/{pair}/depth",
+            query_params,
+        )
+        return await self._api_call(
             url=url,
             method=HTTPMethod.GET,
         )
