b6159686b00225c53a180f01869229258e6036d5
Blessing <sooperfect00@gmail.com>
2024-01-03 02:20:41 +0100

    Add Docstring to Beneficiary and AsyncBeneficiary wrapper

--- pyquidax/wrappers/beneficiary.py
+++ pyquidax/wrappers/beneficiary.py
@@ -5,16 +5,51 @@ from pyquidax.utils import Currency, HTTPMethod
 
 
 class Beneficiary(BaseAPIWrapper):
-    def __init__(self, secret_key: Optional[str] = None):
-        super().__init__(secret_key)
+    """A wrapper for interacting with user accounts on Quidax"""
 
     def all(self, currency: Currency, user_id: str = "me"):
+        """Fetch all beneficiaries for the authenticated user or a sub account
+
+        Args:
+            currency: Currency Allowed valued: btc, ltc, xrp, dash, trx, doge.
+
+            user_id: the user_id of Sub-account linked to the authenticated user
+                for performing actions for a subaccount.
+
+         Returns:
+            APIResponse, which is a dataclass containing the response gotten from Quidax servers.
+            `APIResponse.status_code` (int) is the http status code of the response.
+            `APIResponse.status` (str | None) is the status of the response.
+            `APIResponse.message` (str | None) is the message of the response.
+            `APIResponse.data` (dict | None) is the data returned by Quidax as a result of the
+            request sent.
+
+        """
+
         return self._api_call(
             url=f"{self.base_url}/users/{user_id}/beneficiaries?currency={currency}",
             method=HTTPMethod.GET,
         )
 
     def create(self, currency: Currency, uid: str, extra: str, user_id: str = "me"):
+        """Create a beneficiary account for an authenticated account
+
+        Args:
+             currency: Currency. Allowed valued: btc, ltc, xrp, dash, trx, doge.
+             uid: Wallet Address of your crypto asset.
+             extra: Additional defined label for the account.
+             user_id: The User ID. Use 'me' for main authenticated user.
+             Use the user_id of Sub-account linked to the authenticated user
+                for performing actions on a subaccount.
+
+        Returns:
+            APIResponse, which is a dataclass containing the response gotten from Quidax servers.
+            `APIResponse.status_code` (int) is the http status code of the response.
+            `APIResponse.status` (str | None) is the status of the response.
+            `APIResponse.message` (str | None) is the message of the response.
+            `APIResponse.data` (dict | None) is the data returned by Quidax as a result of the
+            request sent.
+        """
         data = {"currency": currency, "uid": uid, "extra": extra}
         return self._api_call(
             url=f"{self.base_url}/users/{user_id}/beneficiaries",
@@ -23,12 +58,44 @@ class Beneficiary(BaseAPIWrapper):
         )
 
     def get(self, id: str, user_id: str = "me"):
+        """Fetches a beneficiary account for an authenticated user.
+
+        Args:
+            id: The beneficiary id
+            user_id: The User ID. Use 'me' for main authenticated user.
+            Use the user_id of Sub-account linked to the authenticated user for performing actions for a subaccount.
+
+        Returns:
+            APIResponse, which is a dataclass containing the response gotten from Quidax servers.
+            `APIResponse.status_code` (int) is the http status code of the response.
+            `APIResponse.status` (str | None) is the status of the response.
+            `APIResponse.message` (str | None) is the message of the response.
+            `APIResponse.data` (dict | None) is the data returned by Quidax as a result of the
+            request sent.
+        """
         return self._api_call(
             url=f"{self.base_url}/users/{user_id}/beneficiaries/{id}",
             method=HTTPMethod.GET,
         )
 
     def update(self, id: str, user_id: str = "me"):
+        """Update the beneficiary account.
+
+        Args:
+            id: The beneficiary id
+            user_id: Use 'me' for main authenticated user ,use the user_id of Sub-account linked
+                to the authenticated user for performing actions for a subaccount.
+
+        Returns:
+            APIResponse, which is a dataclass containing the response gotten from Quidax servers.
+            `APIResponse.status_code` (int) is the http status code of the response.
+            `APIResponse.status` (str | None) is the status of the response.
+            `APIResponse.message` (str | None) is the message of the response.
+            `APIResponse.data` (dict | None) is the data returned by Quidax as a result of the
+            request sent.
+
+
+        """
         return self._api_call(
             url=f"{self.base_url}/users/{user_id}/beneficiaries/{id}",
             method=HTTPMethod.GET,
@@ -37,6 +104,23 @@ class Beneficiary(BaseAPIWrapper):
 
 class AsyncBeneficiary(BaseAsyncAPIWrapper):
     async def all(self, currency: Currency, user_id: str = "me"):
+        """Fetch all beneficiaries for the authenticated user or a sub account
+
+        Args:
+            currency: Currency Allowed valued: btc, ltc, xrp, dash, trx, doge.
+
+            user_id: the user_id of Sub-account linked to the authenticated user
+            for performing actions for a subaccount.
+
+         Returns:
+            APIResponse, which is a dataclass containing the response gotten from Quidax servers.
+            `APIResponse.status_code` (int) is the http status code of the response.
+            `APIResponse.status` (str | None) is the status of the response.
+            `APIResponse.message` (str | None) is the message of the response.
+            `APIResponse.data` (dict | None) is the data returned by Quidax as a result of the
+            request sent.
+
+        """
         return await self._api_call(
             url=f"{self.base_url}/users/{user_id}/beneficiaries?currency={currency}",
             method=HTTPMethod.GET,
@@ -45,6 +129,23 @@ class AsyncBeneficiary(BaseAsyncAPIWrapper):
     async def create(
         self, currency: Currency, uid: str, extra: str, user_id: str = "me"
     ):
+        """Create a beneficiary account for an authenticated account
+
+        Args:
+             currency: Currency. Allowed valued: btc, ltc, xrp, dash, trx, doge.
+             uid: Wallet Address of your crypto asset.
+             extra: Additional defined label for the account.
+             user_id: The User ID. Use 'me' for main authenticated user.
+             Use the user_id of Sub-account linked to the authenticated user for performing actions on a subaccount.
+
+        Returns:
+            APIResponse, which is a dataclass containing the response gotten from Quidax servers.
+            `APIResponse.status_code` (int) is the http status code of the response.
+            `APIResponse.status` (str | None) is the status of the response.
+            `APIResponse.message` (str | None) is the message of the response.
+            `APIResponse.data` (dict | None) is the data returned by Quidax as a result of the
+            request sent.
+        """
         data = {"currency": currency, "uid": uid, "extra": extra}
         return await self._api_call(
             url=f"{self.base_url}/users/{user_id}/beneficiaries",
@@ -53,12 +154,44 @@ class AsyncBeneficiary(BaseAsyncAPIWrapper):
         )
 
     async def get(self, id: str, user_id: str = "me"):
+        """Fetches a beneficiary account for an authenticated user.
+
+        Args:
+            id: The beneficiary id
+            user_id: The User ID. Use 'me' for main authenticated user.
+            use the user_id of Sub-account linked to the authenticated user for performing actions for a subaccount.
+
+        Returns:
+            APIResponse, which is a dataclass containing the response gotten from Quidax servers.
+            `APIResponse.status_code` (int) is the http status code of the response.
+            `APIResponse.status` (str | None) is the status of the response.
+            `APIResponse.message` (str | None) is the message of the response.
+            `APIResponse.data` (dict | None) is the data returned by Quidax as a result of the
+            request sent.
+        """
         return await self._api_call(
             url=f"{self.base_url}/users/{user_id}/beneficiaries/{id}",
             method=HTTPMethod.GET,
         )
 
     async def update(self, id: str, user_id: str = "me"):
+        """Update the beneficiary account.
+
+        Args:
+            id: The beneficiary id
+            user_id: Use 'me' for main authenticated user.
+            Use the user_id of Sub-account linked to the authenticated user for performing actions for a subaccount.
+
+        Returns:
+            APIResponse, which is a dataclass containing the response gotten from Quidax servers.
+            `APIResponse.status_code` (int) is the http status code of the response.
+            `APIResponse.status` (str | None) is the status of the response.
+            `APIResponse.message` (str | None) is the message of the response.
+            `APIResponse.data` (dict | None) is the data returned by Quidax as a result of the
+            request sent.
+
+
+        """
         return await self._api_call(
             url=f"{self.base_url}/users/{user_id}/beneficiaries/{id}",
             method=HTTPMethod.GET,
